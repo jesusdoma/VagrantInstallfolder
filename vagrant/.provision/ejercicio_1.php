bc18b2d133a1a92ae858a099dcd8835d965948ce
jesusdoma <MEGATRIX12@gmail.com>
2020-10-12 19:27:52 +0200

    Initial commit

---
+++ vagrant/.provision/ejercicio_1.php
@@ -0,0 +1,23 @@
+<?php
+    $uno=1;
+    $dos="dos";
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <p>
+            <?php
+                echo"variable 'uno'= $uno <br>
+                variable 'dos'= $dos";
+            ?>
+        </p>
+    </body>
+</html>
